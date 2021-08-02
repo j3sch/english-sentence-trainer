96c7142f37f78a51114a9b97224debec8b7d0b87
js414 <js414@hdm-stuttgart.de>
2021-08-02 23:59:18 +0200

    cimport in api exercises

--- src/pages/api/exercises.js
+++ src/pages/api/exercises.js
@@ -1,4 +1,4 @@
-import { connectToDatabase } from '~/utils/mongodb';
+import connectToDatabase from '~/utils/mongodb';
 
 export default async function handler(req, res) {
 	const { db } = await connectToDatabase();
