6abef2e1a38b43de91dca7e9236fbfec38cd9f42
js414 <js414@hdm-stuttgart.de>
2021-08-02 23:43:15 +0200

    api exercises added again

---
+++ src/pages/api/exercises.js
@@ -0,0 +1,11 @@
+import { connectToDatabase } from '~/utils/mongodb';
+
+export default async function handler(req, res) {
+	const { db } = await connectToDatabase();
+
+	const data = req.query;
+
+	const response = await db.collection('exercises').insertOne({ data });
+
+	res.json(response);
+}
