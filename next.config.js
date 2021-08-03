5c11e259fd0f408d1d07979c5ab9887e3b5712b9
js414 <js414@hdm-stuttgart.de>
2021-08-03 12:48:52 +0200

    cors added

--- next.config.js
+++ next.config.js
@@ -4,4 +4,28 @@ module.exports = {
 		// your project has ESLint errors.
 		ignoreDuringBuilds: true,
 	},
+	async headers() {
+		return [
+			{
+				// matching all API routes
+				source: '/api/exercises',
+				headers: [
+					{ key: 'Access-Control-Allow-Credentials', value: 'true' },
+					{
+						key: 'Access-Control-Allow-Origin',
+						value: 'https://english-sentence-trainer.vercel.app/',
+					},
+					{
+						key: 'Access-Control-Allow-Methods',
+						value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+					},
+					{
+						key: 'Access-Control-Allow-Headers',
+						value:
+							'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+					},
+				],
+			},
+		];
+	},
 };
